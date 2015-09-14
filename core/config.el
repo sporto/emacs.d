b74a265d85baf9a5a8299e48ea299bf9a4eed33b
Sebastian Porto <sebasporto@gmail.com>
2015-09-14 21:43:09 +1000

    Added elm

--- core/config.el
+++ core/config.el
@@ -1,16 +1,16 @@
 ; JSX
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-      (let ((web-mode-enable-part-face nil))
-        ad-do-it)
-    ad-do-it))
+; (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;   (if (equal web-mode-content-type "jsx")
+;       (let ((web-mode-enable-part-face nil))
+;         ad-do-it)
+;     ad-do-it))
+
+; (require 'jsx-mode)
+; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
 
 ; Display line numbers
 (add-hook 'prog-mode-hook 'linum-mode)
-
-(require 'jsx-mode)
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
 
 ; (require 'helm-config)
 
--- core/packages.el
+++ core/packages.el
@@ -7,20 +7,23 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+; (el-get-self-update)
 
 ;; Simple package names
 (el-get-bundle ace-jump-mode)
 (el-get-bundle ace-window)
 (el-get-bundle flycheck)
-(el-get-bundle jsx-mode)
+; (el-get-bundle jsx-mode)
 (el-get-bundle neotree)
 (el-get-bundle projectile)
 (el-get-bundle smart-mode-line)
 ; (el-get-bundle drag-stuff)
 ; (el-get-bundle helm)
 
-; (el-get-self-update)
+(el-get-bundle elm-mode
+  :url "https://github.com/jcollard/elm-mode.git")
+
 (el-get 'sync)
 
 ; Activate SML
